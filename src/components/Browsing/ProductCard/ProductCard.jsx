bf1be1975ed4b91eaf054d412156d7c182badc93
Josh Klope <klope3@msn.com>
2022-12-18 11:23:41 -0600

    Refactor product filtering

--- src/components/Browsing/ProductCard/ProductCard.jsx
+++ src/components/Browsing/ProductCard/ProductCard.jsx
@@ -22,8 +22,8 @@ class ProductCard extends React.Component {
                 {/* <div>{attributes.find(attribute => attribute.name === "File Formats").value.map(item => <div key={item.label}>{item.label}</div>)}</div> */}
                 {/* <div>Poly count: {attributes.find(attribute => attribute.name === "Polygon Count").value}</div> */}
                 {/* <div>Publication Date: {attributes.find(attribute => attribute.name === "Publication Date").value}</div> */}
-                <div>Game Ready? {attributes.find(attribute => attribute.name === "Game Ready").value ? "True" : "False"}</div>
-                <div>Animation Ready? {attributes.find(attribute => attribute.name === "Animation Ready").value ? "True" : "False"}</div>
+                {/* <div>Game Ready? {attributes.find(attribute => attribute.name === "Game Ready").value ? "True" : "False"}</div> */}
+                {/* <div>Animation Ready? {attributes.find(attribute => attribute.name === "Animation Ready").value ? "True" : "False"}</div> */}
             </div>
         )
     }
--- src/components/Browsing/ProductList/ProductList.jsx
+++ src/components/Browsing/ProductList/ProductList.jsx
@@ -3,81 +3,59 @@ import ProductCard from "../ProductCard/ProductCard";
 import "./ProductList.css";
 
 class ProductList extends React.Component {
-    objectHasAnyTruthyValue = object => {
-        // console.log("Checking for truthy values in the following object");
-        // console.log(object);
-        // if (Object.keys(object).find(key => object[key] != false)) console.log("Has at least one truthy value.");
-        return Object.keys(object).find(key => object[key] != false);
-    }
+    objectHasAnyTruthyValue = object => Object.keys(object).find(key => object[key] != false);
     getProductAttribute = (product, attributeName) => product.attributes.find(attribute => attribute.name === attributeName);
-    shouldUseFilter = (filterName, filters) => {
-        // console.log("Checking whether to use the attribute " + attributeName);
-        // console.log(Object.keys(filters).includes(attributeName) + ", " + (filters[attributeName] && this.objectHasAnyTruthyValue(filters[attributeName])));
-        // if (!Object.keys(filters).includes(attributeName)) console.log("Not filtering for attribute " + attributeName + " because no object with that data was found in filters.");
-        // else if (!this.objectHasAnyTruthyValue(filters[attributeName])) console.log("Not filtering for attribute " + attributeName + " because the corresponding object has no truthy values.");
-        return Object.keys(filters).includes(filterName) && this.objectHasAnyTruthyValue(filters[filterName]);
+    shouldUseFilter = (filterName, filterInputs) => {
+        return Object.keys(filterInputs).includes(filterName) && this.objectHasAnyTruthyValue(filterInputs[filterName]);
+    }
+    isInNumberRange = (value, minMaxObject) => {
+        const { min: givenMin, max: givenMax } = minMaxObject;
+        const min = givenMin === undefined ? 0 : parseInt(givenMin);
+        const max = givenMax === undefined || parseInt(givenMax) <= 0 ? Number.MAX_VALUE : parseInt(givenMax);
+        return (value >= min && value <= max);
     }
 
-    filterProducts = (products, filters) => {
+    filterProducts = (products, filterInputs) => {
         let result = products;
-        if (this.shouldUseFilter("Author", filters)) {
-            result = products.filter(product => {
-                const authorOfProduct = this.getProductAttribute(product, "Author").value[0].value;
-                return filters["Author"][authorOfProduct];
-            });
-        }
 
-        if (this.shouldUseFilter("File Formats", filters)) {
-            result = result.filter(product => {
-                const productFormatLabels = this.getProductAttribute(product, "File Formats").value.map(item => item.label);
+        const filterFunctions = {
+            "Author": product => {
+                const name = "Author";
+                const productAuthor = this.getProductAttribute(product, name).value[0].value;
+                return filterInputs[name][productAuthor];
+            },
+            "File Formats": product => {
+                const name = "File Formats";
+                const productFormatLabels = this.getProductAttribute(product, name).value.map(item => item.label);
                 for (const i in productFormatLabels) {
-                    if (filters["File Formats"][productFormatLabels[i]]) {
+                    if (filterInputs[name][productFormatLabels[i]]) {
                         return true;
                     }
                 }
                 return false;
-            });
-        }
-        
-        if (this.shouldUseFilter("Polygon Count", filters)) {
-            result = result.filter(product => {
-                const productPolygons = this.getProductAttribute(product, "Polygon Count").value;
-                const { min: givenMin, max: givenMax } = filters["Polygon Count"];
-                const min = givenMin === undefined ? 0 : parseInt(givenMin);
-                const max = givenMax === undefined || parseInt(givenMax) <= 0 ? Number.MAX_VALUE : parseInt(givenMax);
-                return (productPolygons >= min && productPolygons <= max);
-            });
-        }
-
-        if (this.shouldUseFilter("Publication Date", filters)) {
-            result = result.filter(product => {
-                const productDate = this.getProductAttribute(product, "Publication Date").value;
-                const { earliest: givenEarliest, latest: givenLatest } = filters["Publication Date"];
+            },
+            "Polygon Count": product => {
+                const name = "Polygon Count";
+                const productPolygons = this.getProductAttribute(product, name).value;
+                return this.isInNumberRange(productPolygons, filterInputs[name]);
+            },
+            "Publication Date": product => {
+                const name = "Publication Date";
+                const productDate = this.getProductAttribute(product, name).value;
+                const { earliest: givenEarliest, latest: givenLatest } = filterInputs[name];
                 const earliest = givenEarliest === undefined ? "0" : givenEarliest;
                 const latest = givenLatest === undefined ? "9999/99/99" : givenLatest;
                 return productDate >= earliest && productDate <= latest;
-            });
+            },
+            "Game Ready": product => this.getProductAttribute(product, "Game Ready").value,
+            "Animation Ready": product => this.getProductAttribute(product, "Animation Ready").value,
+            "price": product => this.isInNumberRange(product.price.raw, filterInputs["price"]),
         }
 
-        if (this.shouldUseFilter("Game Ready", filters)) {
-            result = result.filter(product => this.getProductAttribute(product, "Game Ready").value);
+        for (const key in filterInputs) {
+            if (!this.shouldUseFilter(key, filterInputs)) continue;
+            result = result.filter(product => filterFunctions[key](product));
         }
-
-        if (this.shouldUseFilter("Animation Ready", filters)) {
-            result = result.filter(product => this.getProductAttribute(product, "Animation Ready").value);
-        }
-
-        if (this.shouldUseFilter("price", filters)) {
-            result = result.filter(product => {
-                const price = product.price.raw;
-                const { min: givenMin, max: givenMax } = filters["price"];
-                const min = givenMin === undefined ? 0 : parseInt(givenMin);
-                const max = givenMax === undefined || parseInt(givenMax) <= 0 ? Number.MAX_VALUE : parseInt(givenMax);
-                return (price >= min && price <= max);
-            });
-        }
-
-
 
         return result;
     } //will need custom implementation based on specific products, attributes, use case
